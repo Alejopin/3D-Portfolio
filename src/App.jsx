78d244ee78d2ecba7367ccc4988cb16d1af9fa79
Alejopin <alejopin.dev@gmail.com>
2024-11-08 01:04:12 -0300

    fix

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello Vite!</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import reactDOM from 'react-router-dom'
+import App from './App.jsx'
+
+
+reactDOM.createRoot(document.getElementById('root')).render(<App />)
